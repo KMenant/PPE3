ef416b1e31f7f914c21daeac54747af30920c429
Kévin Menant <menant.kevin@hotmail.fr>
2015-10-02 18:00:02 +0200

    BDD

--- alae.sql
+++ alae.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Sam 10 Janvier 2015 à 17:33
+-- Généré le :  Ven 02 Octobre 2015 à 17:33
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -43,6 +43,23 @@ INSERT INTO `administrateur` (`id`, `identifiant`, `mp`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `enfant`
+--
+
+CREATE TABLE IF NOT EXISTS `enfant` (
+  `id_enfant` int(3) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) COLLATE utf8_bin NOT NULL,
+  `prenom` varchar(255) COLLATE utf8_bin NOT NULL,
+  `specificite` varchar(255) COLLATE utf8_bin NOT NULL,
+  `commentaire` varchar(255) COLLATE utf8_bin NOT NULL,
+  `id_famille` int(3) NOT NULL,
+  PRIMARY KEY (`id_enfant`),
+  KEY `id_famille` (`id_famille`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `famille`
 --
 
@@ -75,9 +92,40 @@ CREATE TABLE IF NOT EXISTS `famille` (
   `date_envoi_mail_demande_inscription` date DEFAULT NULL,
   PRIMARY KEY (`id_famille`),
   UNIQUE KEY `identifiant` (`identifiant`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Contenu de la table `famille`
+-- Structure de la table `personnel`
 --
 
+CREATE TABLE IF NOT EXISTS `personnel` (
+  `id_personnel` int(3) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) COLLATE utf8_bin NOT NULL,
+  `prenom` varchar(255) COLLATE utf8_bin NOT NULL,
+  `login` varchar(255) COLLATE utf8_bin NOT NULL,
+  `mdp` varchar(255) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id_personnel`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `personnel`
+--
+
+INSERT INTO `personnel` (`id_personnel`, `nom`, `prenom`, `login`, `mdp`) VALUES
+(001, 'Rince', 'Camille', 'crince', '84675f2baf7140037b8f5afe54eef841');
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `enfant`
+--
+ALTER TABLE `enfant`
+  ADD CONSTRAINT `enfant_ibfk_1` FOREIGN KEY (`id_famille`) REFERENCES `famille` (`id_famille`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
